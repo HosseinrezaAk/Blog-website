e4216abd38342d0712cddb47f00a54fa3cca0f87
Hosseinreza <hrakbari77@gmail.com>
2023-02-06 11:42:44 +0330

    Add get for home

--- app/app.js
+++ app/app.js
@@ -1,5 +1,3 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -15,7 +13,9 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-
+app.get("/", function( req, res ){
+  res.render("home");
+});
 
 
 
@@ -29,5 +29,5 @@ app.use(express.static("public"));
 
 
 app.listen(3000, function() {
-  console.log("Server started on port 3000");
+  console.log("Server started on http://localhost:3000");
 });
